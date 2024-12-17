97efbb5b66cee8b1a2e463e7684325eb8473ec7c
theo <boronattheo83@gmail.com>
2024-12-17 16:39:05 +0100

    first nav without css

---
+++ client/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,15 @@
+import LogoMenu from "../../public/manette.png";
+import LogoConnexion from "../../public/fantome.png";
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <nav>
+      <img id="menunav" src={LogoMenu} alt="logo menu nav" />
+      <p id="appname">WILDY GAMY</p>
+      <img id="connexionnav" src={LogoConnexion} alt="logo connexion nav" />
+    </nav>
+  );
+}
+
+export default NavBar;
