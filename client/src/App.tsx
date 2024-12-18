f98f91a9007f4d245aaff9048f3c4e5a5bc5ba5d
theo <boronattheo83@gmail.com>
2024-12-18 14:53:48 +0100

    resolve conflicts and merge

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import NavBar from "./components/NavBar/NavBar";
 import { Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
