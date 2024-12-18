41227e855bb6e41a2976e4ccaf5c639c47e2e40b
elhedi <hheeddii87@gmail.com>
2024-12-18 14:36:15 +0100

    s1us10FooterSombre

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import pacman from "../../public/pacman.png";
+import "./Footer.css";
+export default function Footer() {
+  return (
+    <footer className="footer-sombre">
+      <img src={pacman} alt="" />
+      <img src={pacman} alt="" />
+      <img src={pacman} alt="" />
+      <p>Adresse et fait par les Wilders</p>
+      <img src={pacman} alt="" />
+      <img src={pacman} alt="" />
+      <img src={pacman} alt="" />
+    </footer>
+  );
+}
