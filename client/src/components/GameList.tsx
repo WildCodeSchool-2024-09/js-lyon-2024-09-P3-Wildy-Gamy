5e80b3e08471c31da295c02970dbbc6ac50212bf
elhedi <hheeddii87@gmail.com>
2024-12-18 11:47:07 +0100

    mise en place du composant gamelist

--- client/src/components/GameList.tsx
+++ client/src/components/GameList.tsx
@@ -1,11 +1,23 @@
-/*import { ReactNode } from "react";
+import { Link } from "react-router-dom";
 
-type GameData = {
-  name: string;
-  principle: string;
-};
-
-export default function GameList() {
-  return <></>;
+interface GameListProps {
+  data: {
+    id: number;
+    name: string;
+    image: string;
+  };
 }
-*/
+
+export default function GameList({ data }: GameListProps) {
+  return (
+    <>
+      <div>
+        <h1>{data.name}</h1>
+        <img src={data.image} alt="" />
+        <Link to={`/api/games/${data.id}`}>
+          <button type="button">Details</button>
+        </Link>
+      </div>
+    </>
+  );
+}
