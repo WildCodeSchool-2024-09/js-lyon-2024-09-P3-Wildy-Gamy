f5c26f7e3c268910b64fbf16a8cd838f34d2e414
DameDanny <161458137+DameDanny@users.noreply.github.com>
2025-01-14 10:47:59 +0100

    Création tests game + correction erreur gameaction

--- server/tests/game/routes.spec.ts
+++ server/tests/game/routes.spec.ts
@@ -33,3 +33,40 @@ describe("GET /api/games", () => {
     expect(response.body).toStrictEqual(rows);
   });
 });
+
+// Test suite for the GET /api/games/:id route
+describe("GET /api/games/:id", () => {
+  it("should fetch a single item successfully", async () => {
+    // Mock rows returned from the database
+    const rows = [{}] as Rows;
+
+    // Mock the implementation of the database query method
+    jest
+      .spyOn(databaseClient, "query")
+      .mockImplementation(async () => [rows, []]);
+
+    // Send a GET request to the /api/games/:id endpoint
+    const response = await supertest(app).get("/api/games/1");
+
+    // Assertions
+    expect(response.status).toBe(200);
+    expect(response.body).toStrictEqual(rows[0]);
+  });
+
+  it("should fail on invalid id", async () => {
+    // Mock empty rows returned from the database
+    const rows = [] as Rows;
+
+    // Mock the implementation of the database query method
+    jest
+      .spyOn(databaseClient, "query")
+      .mockImplementation(async () => [rows, []]);
+
+    // Send a GET request to the /api/games/:id endpoint with an invalid ID
+    const response = await supertest(app).get("/api/games/0");
+
+    // Assertions
+    expect(response.status).toBe(404);
+    expect(response.body).toEqual({});
+  });
+});
