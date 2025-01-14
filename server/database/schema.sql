579b9e06e93d265dd6b42e707db3facc1fd46805
elhedi <hheeddii87@gmail.com>
2025-01-14 12:01:06 +0100

    peuplage des tables lots et user

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -20,6 +20,11 @@ create table lots (
   image varchar(255) null
 );
 
+insert into lots (id,name,nb_lots,nb_points_needed,image)
+values
+(1,"anneau_de_pouvoir",5,150,"https://e7.pngegg.com/pngimages/837/290/png-clipart-the-lord-of-the-rings-the-fellowship-of-the-ring-sauron-one-ring-lord-of-the-rings-ring-gold.png"),
+(2,"pokeball",5,200,"https://w7.pngwing.com/pngs/801/726/png-transparent-pokemon-pokeball-nintendo-ball-thumbnail.png");
+
 create table user (
   id int unsigned primary key auto_increment not null,
   pseudo varchar(150) not null,
@@ -28,3 +33,8 @@ create table user (
   password varchar(100) not null,
   image varchar(250) null
 );
+
+insert into user (id,pseudo,points,email,password,image)
+values
+(1,"toto",0,"thomas-osana_student2024@wilder.school","123456789","https://upload.wikimedia.org/wikipedia/commons/c/c0/Z%C3%A9ro_plus_z%C3%A9ro_%C3%A9gale_la_t%C3%AAte_%C3%A0_TOTO.png"),
+(2,"dammedanny",200,"email@email.com","eU7HzL56P//?","https://png.pngtree.com/png-vector/20241124/ourlarge/pngtree-charming-anime-character-with-a-sweet-smile-png-image_14191640.png");
