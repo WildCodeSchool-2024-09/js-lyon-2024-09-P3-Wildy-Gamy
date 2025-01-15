075f0c348d438bb0ff9a7aa87b1fe1d012366d7e
DameDanny <161458137+DameDanny@users.noreply.github.com>
2025-01-15 16:50:01 +0100

    Addition jeux à bdd et modification menu burger pour ne pas l'encombrer

--- client/src/components/MenuBurger.tsx
+++ client/src/components/MenuBurger.tsx
@@ -1,22 +1,9 @@
 import Hamburger from "hamburger-react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-
-interface gameProps {
-  id: number;
-  name: string;
-}
 
 export default function Menu() {
   const [open, setOpen] = useState(false);
-  const [listGames, setListGames] = useState<gameProps[]>([]);
-  useEffect(() => {
-    fetch(`${import.meta.env.VITE_API_URL}/api/games`)
-      .then((res) => res.json())
-      .then((data) => {
-        setListGames(data);
-      });
-  }, []);
   return (
     <div>
       <Hamburger size={28} toggled={open} toggle={setOpen} />
@@ -34,11 +21,6 @@ export default function Menu() {
           <li>
             <Link to="/Lots">Lots</Link>
           </li>
-          {listGames.map((game) => (
-            <li key={game.id}>
-              <Link to={`/Game/${game.id}`}>Page {game.name}</Link>
-            </li>
-          ))}
         </ul>
       )}
     </div>
--- server/database/schema.sql
+++ server/database/schema.sql
@@ -10,9 +10,14 @@ create table game (
 insert into game (id,name,principle,in_room,is_playable,image)
 values
   (1,"PacMan","Mange des boules en evitant des fantomes",true,true,"https://m.media-amazon.com/images/I/71IH4Pvl7rL._AC_UF894,1000_QL80_.jpg"),
-  (2,"Mario","Plombier perdu dans un monde sous-terrain qui cherche a sauver son ami Luigi",true,false,"https://prod-printler-front-as.azurewebsites.net/media/photo/166695.jpg?mode=crop&width=638&height=900&rnd=0.0.1"),
-  (3, "Tekken","Des personnes de différentes origines se battent jusqu'à la mort.", true, false,""),
-  (4, "Pinball", "Jouez un vaisseau à travers l'univers.", true, false, "");
+  (2,"Mario","Plombier perdu dans un monde sous-terrain qui cherche a sauver son frère Luigi",true,false,"https://prod-printler-front-as.azurewebsites.net/media/photo/166695.jpg?mode=crop&width=638&height=900&rnd=0.0.1"),
+  (3, "Tekken","Des personnes de différentes origines se battent jusqu'à la mort.", true, false,"https://i.pinimg.com/736x/c1/3e/11/c13e11a50de147247c595b04f7748696.jpg"),
+  (4, "Pinball", "Jouez un vaisseau à travers l'univers.", true, false, "https://i.pinimg.com/736x/90/06/40/900640bc50df22019c5f54c21baf24db.jpg"),
+  (5, "Space Invaders", "Défendez-vous contre les aliens qui cherchent à envahir votre planète!", true, false, "https://i.pinimg.com/736x/36/64/e6/3664e640f1dee9dd236237dd4d48ec76.jpg"),
+  (6, "Donkey Kong", "Jouez Mario et essayez de sauver votre amie contre Donkey Kong.", true, false, "https://i.pinimg.com/736x/8c/98/f7/8c98f777d9aba20bac28fb702c03c3d2.jpg"),
+  (7, "Megaman", "Vous jouer un petit robot qui cherche à sauver sa planète.", true, false, "https://i.pinimg.com/736x/77/8e/e8/778ee8ab0131c732518b6032bce9ac52.jpg"),
+  (8, "Castlevania", "Allez dans le manoir de Dracula et empêchez-le de détruire la race humaine.", true, false, "https://i.pinimg.com/736x/aa/24/f6/aa24f6888d328d6d647f8d5a90dd86f4.jpg"),
+  (9, "La légende de Zelda", "Vous jouez Link, un chevalier et cherchez à sauvez la princesse Zelda et le royaume d'Hyrule de la tyrannie de Ganon", true, false, "https://i.pinimg.com/736x/f2/ea/5a/f2ea5a3f1c432e6233deb1e7f0c4ce66.jpg");
 
 create table lots (
   id int unsigned primary key auto_increment not null,
